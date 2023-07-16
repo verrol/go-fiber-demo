ab76ac72cdc2dc4dca3ec977b1973b7b2c62913e
Verrol Adams <verrol@lorrev.org>
2023-07-16 06:33:37 -0400

    Demonstrates how easy it is to write and use a Middleware.

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Use(logMiddleware)
 	app.Get("/", hello)
+
+	app.Use(addHeaderMiddleware)
+	app.Get("/api", anApi)
+
 
 	log.Info("starting server", "port", 8080)
 	err := app.Listen(":8080")
@@ -16,6 +21,22 @@ func main() {
 	}
 }
 
+// logMiddleware is a simple logging middleware
+func logMiddleware(c *fiber.Ctx) error {
+	log.Info("my middleware called", "path", c.Path(),  "method", c.Method())
+	return c.Next()
+}
+
+// addHeaderMiddleware adds a header to the response
+func addHeaderMiddleware(c *fiber.Ctx) error {
+	c.Set("X-Header", "Custom header")
+	return c.Next()
+}
+
 func hello(c *fiber.Ctx) error {
-	return c.SendString("Hello World!")
+	return c.SendString("Hello, World!")
+}
+
+func anApi(c *fiber.Ctx) error {
+	return c.SendString("this endpoint called after middleware")
 }
